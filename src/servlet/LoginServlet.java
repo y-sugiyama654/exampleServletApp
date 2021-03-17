e86730bc1183cd5b357f49f17ce84703ab732dd9
Yuta Sugiyama <y.sugiyama54@gmail.com>
2021-03-18 03:53:16 +0900

    POST時のログイン処理内の修正

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Login;
+import model.LoginLogic;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -19,8 +23,32 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginOK.jsp");
-		dispatcher.forward(request, response);
+
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+
+		// ログイン処理の実行
+		Login login = new Login(userId, pass);
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(login);
+
+		// ログイン処理の成否によって処理を分岐
+		if (result) {
+			// ログイン成功時
+			// セッションスコープにユーザーIDを保存
+			HttpSession session = request.getSession();
+			session.setAttribute("userId", userId);
+
+			// フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginOK.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			// ログイン失敗時
+			// リダイレクト
+			response.sendRedirect("/example/LoginServlet");
+		}
 	}
 
 }
